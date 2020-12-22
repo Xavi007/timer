d4379ad44824dd0a8ef0d12d9d6fc7830e5cd22f
savitoj <savitoj.cheema@gmail.com>
2020-12-22 20:02:36 +0530

    log icon and image urls

--- src/utils/notifications/index.js
+++ src/utils/notifications/index.js
@@ -3,11 +3,16 @@ export const timeUpNotification = () => {
   baseUrl += "//";
   baseUrl += window.location.hostname;
 
+  const iconUrl = new URL("/notifications/timer_icon.png", baseUrl);
+  const imageUrl = new URL("/notifications/image.jpeg", baseUrl);
+
   console.log("BASE URL", baseUrl);
+  console.log("ICON URL", iconUrl);
+  console.log("IMAGE URL", imageUrl);
 
   return new Notification("Times Up!", {
     body: "You already know man.",
-    icon: new URL("/notifications/timer_icon.png", baseUrl),
-    image: new URL("/notifications/image.jpeg", baseUrl),
+    icon: iconUrl,
+    image: imageUrl,
   });
 };
