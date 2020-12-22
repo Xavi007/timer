433b98f60deee224ac55dbacb2b6a8683f51d818
savitoj <savitoj.cheema@gmail.com>
2020-12-22 19:02:38 +0530

    javascript errors fixed

--- src/utils/notifications/index.js
+++ src/utils/notifications/index.js
@@ -1,6 +1,3 @@
-import timer_up_icon from "./timer_icon.png";
-import timer_up_image from "./image.jpeg";
-
 export const timeUpNotification = () =>
   new Notification("Times Up!", {
     body: "You already know man.",
