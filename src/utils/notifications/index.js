43b9cb7b6e1e01226d5c0dbc0b4f8f294b26d077
savitoj <savitoj.cheema@gmail.com>
2020-12-22 19:39:10 +0530

    new base url

--- src/utils/notifications/index.js
+++ src/utils/notifications/index.js
@@ -1,11 +1,13 @@
 export const timeUpNotification = () => {
-  const baseUrl = window.location.hostname;
+  const baseUrl = window.location.protocol;
+  baseUrl += "//";
+  baseUrl += window.location.hostname;
 
   console.log("BASE URL", baseUrl);
 
   return new Notification("Times Up!", {
     body: "You already know man.",
-    icon: new URL("/notifications/timer_icon.png", window.location.hostname),
-    image: new URL("/notifications/image.jpeg", window.location.hostname),
+    icon: new URL("/notifications/timer_icon.png", baseUrl),
+    image: new URL("/notifications/image.jpeg", baseUrl),
   });
 };
