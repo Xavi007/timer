1b8499c2cbd0dc4e5603ef8b37fb81ba15c15bde
savitoj <savitoj.cheema@gmail.com>
2020-12-22 17:38:06 +0530

    time up notification constructor fix

--- src/utils/notifications/index.js
+++ src/utils/notifications/index.js
@@ -2,7 +2,7 @@ import timer_up_icon from "./timer_icon.png";
 import timer_up_image from "./image.jpeg";
 
 export const timeUpNotification = () =>
-  Notification("Times Up!", {
+  new Notification("Times Up!", {
     body: "You already know man.",
     icon: timer_up_icon,
     image: timer_up_image,
